fc04cc3c682dc5d95ca4176e785c3802657af001
Breno Colaço <brenocolaco@unifor.br>
2022-06-22 23:29:44 -0300

    search doesnt work

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,44 +7,50 @@ import SearchBar from "../../components/SearchBar";
 function Home() {
   const navigate = useNavigate();
   const [pokemon, setPokemon] = useState([]);
-  const [renderData, setRenderData] = useState([])
-  const [search, setSearch] = useState("")
+  const [renderData, setRenderData] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    if (search != "") {
+      console.log("renderData:", renderData);
+      setRenderData(pokemon.filter((poke) => poke.includes(search)));
+      console.log(pokemon.filter((poke) => poke.includes(search)));
+    } else {
+      setRenderData(pokemon);
+    }
+  }, [search]);
   useEffect(() => {
     api
-      .get("/pokemon/?limit=100000&offset=0") //100000
+      .get("/pokemon/?limit=20&offset=0") //100000
       .then((res) => {
-        
         setPokemon([res.data.results]);
       })
       .catch((err) => console.log(err));
   }, []);
-  function SearchPokemon(){
-    api.get(`/pokemon/${search}`).then((res)=> {
-      setPokemon([res.data]);
-      
-    }).catch((err)=> {
-      if(err.response.status == 404){
-        alert("Pokemon não encontrado")
-      }
-    })}
+  function SearchPokemon() {
+    api
+      .get(`/pokemon/${search}`)
+      .then((res) => {
+        setPokemon([res.data]);
+      })
+      .catch((err) => {
+        if (err.response.status == 404) {
+          alert("Pokemon não encontrado");
+        }
+      });
+  }
   return (
     <Cardbox>
       <SearchBar setSearch={setSearch} />
-      <button onClick={()=> SearchPokemon()} >Search</button>
       <Container>
-        
-        {
-         pokemon.map((slices) => {
+        {renderData.map((slices) => {
           let types;
           return slices.map((pokemon) => {
             api.get(`${pokemon.url.split("v2")[1]}`).then((res) => {
               types = res.data.types;
-              console.log("types", types);
             });
             return (
               <PokeCard
                 onClick={() =>
-
                   navigate(`/pokemon/${pokemon.url.split("/")[6]}`)
                 }
                 name={pokemon.name}
@@ -56,8 +62,6 @@ function Home() {
             );
           });
         })}
-              
-        
       </Container>
     </Cardbox>
   );
